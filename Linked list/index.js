b21d1d19cba11c1dc7680c8972bee6ff12f0e312
unknown <allansichangi35@gmail.com>
2022-01-10 01:24:06 +0300

    Linked list file

--- Linked list/index.js
+++ Linked list/index.js
@@ -63,6 +63,28 @@ class Linkedlist {
 		this.size--;
 		return data;
 	}
+
+	removeLast() {
+		if (this.size === 0) {
+			return null;
+		}
+		let data = this.tail.data;
+
+		if (this.size === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			let curr = this.head;
+
+			while (curr.next.next != null) {
+				curr = curr.next;
+			}
+			curr.next = null;
+			this.tail = curr;
+			this.size--;
+			return data;
+		}
+	}
 }
 
 let linkedlist = new Linkedlist();
@@ -70,5 +92,11 @@ linkedlist.append(10);
 linkedlist.append(2);
 linkedlist.prepend(5);
 console.log(linkedlist.printList());
-console.log(linkedlist.removeFirst());
+// console.log(linkedlist.removeFirst());
+console.log(linkedlist.printList());
+console.log(linkedlist.removeLast());
+console.log(linkedlist.printList());
+console.log(linkedlist.removeLast());
+console.log(linkedlist.printList());
+console.log(linkedlist.removeLast());
 console.log(linkedlist.printList());
