c0510e1226891196b10824190f2c3d10465514db
unknown <allansichangi35@gmail.com>
2022-01-12 01:46:40 +0300

    Linked list file

--- Stack/arrayStack.js
+++ Stack/arrayStack.js
@@ -8,4 +8,30 @@ class Stack {
 		this.top++;
 		this.storage[this.top] = data;
 	}
+
+	pop() {
+		if (this.top == -1) {
+			return null;
+		}
+
+		let data = this.storage[this.top];
+		this.top--;
+		return data;
+	}
+
+	getSize() {
+		return this.top + 1;
+	}
+
+	peek() {
+		let data = this.storage[this.top];
+		return data || null;
+	}
 }
+
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack);
+console.log(stack.getSize());
