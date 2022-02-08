8fff7c52eaef64112f347e970c05064c873c44b5
unknown <allansichangi35@gmail.com>
2022-02-08 03:14:12 +0300

    parens

---
+++ Stack/validParenthesis.js
@@ -0,0 +1,17 @@
+var isValid = function (s) {
+	const map = { "(": ")", "{": "}", "[": "]" };
+	let stack = [];
+
+	for (let char of s) {
+		if (map[char]) {
+			stack.push(map[char]);
+		} else if (stack.length > 0 && stack[stack.length - 1] === char) {
+			stack.pop();
+		} else {
+			return false;
+		}
+	}
+	return stack.length == 0;
+};
+
+console.log(isValid("[](){}({})"));
